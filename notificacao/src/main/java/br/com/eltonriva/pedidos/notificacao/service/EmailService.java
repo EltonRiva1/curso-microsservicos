d127bb699b9a70e037fa5ffd3b77ad70e0663a1b
Elton Riva <notleamil@hotmail.com>
2025-10-28 10:47:28 -0300

    git commit -m "♻️ adicionada interface"

--- notificacao/src/main/java/br/com/eltonriva/pedidos/notificacao/service/EmailService.java
+++ notificacao/src/main/java/br/com/eltonriva/pedidos/notificacao/service/EmailService.java
@@ -1,32 +1,7 @@
 package br.com.eltonriva.pedidos.notificacao.service;
 
-import br.com.eltonriva.pedidos.notificacao.entity.Pedido;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+import br.com.eltonriva.pedidos.notificacao.dto.PedidoEventDto;
 
-@Service
-public class EmailService {
-    private final JavaMailSender javaMailSender;
-
-    public EmailService(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
-    }
-
-    public void enviarEmail(Pedido pedido) {
-        var simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("pedidos-api@company.com");
-        simpleMailMessage.setTo(pedido.getEmailNotificacao());
-        simpleMailMessage.setSubject("Pedido de Compra");
-        simpleMailMessage.setText(this.gerarMensagem(pedido));
-        this.javaMailSender.send(simpleMailMessage);
-    }
-
-    private String gerarMensagem(Pedido pedido) {
-        var pedidoId = pedido.getId().toString();
-        var cliente = pedido.getCliente();
-        var valor = String.valueOf(pedido.getValorTotal());
-        var status = pedido.getStatus().name();
-        return String.format("Olá, %s, seu pedido de número %s no valor de %s, foi realizado com sucesso.\nStatus: %s.", cliente, pedidoId, valor, status);
-    }
+public interface EmailService {
+    void enviarEmail(PedidoEventDto pedido, String correlationId);
 }
