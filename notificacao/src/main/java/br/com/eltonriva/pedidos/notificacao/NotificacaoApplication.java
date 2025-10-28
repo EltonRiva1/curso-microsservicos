012efa4da345ad9ea00adbab84c6c487fd514cba
Elton Riva <notleamil@hotmail.com>
2025-10-28 11:01:12 -0300

    git commit -m "➖ removendo dependência"

--- notificacao/src/main/java/br/com/eltonriva/pedidos/notificacao/NotificacaoApplication.java
+++ notificacao/src/main/java/br/com/eltonriva/pedidos/notificacao/NotificacaoApplication.java
@@ -1,11 +1,9 @@
 package br.com.eltonriva.pedidos.notificacao;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableRabbit
 public class NotificacaoApplication {
 
 	public static void main(String[] args) {
